6e998933709c597a9b2a2be723c5e0d7ed9318a9
santiagotestea2 <55399958+santiagotestea2@users.noreply.github.com>
2019-09-16 12:16:56 -0300

    Update game.py

--- hangman/game.py
+++ hangman/game.py
@@ -1,3 +1,4 @@
+import random
 from .exceptions import *
 
 # Complete with your own, just for fun :)
@@ -5,15 +6,52 @@ LIST_OF_WORDS = []
 
 
 def _get_random_word(list_of_words):
-    pass
+    if not list_of_words:
+        raise InvalidListOfWordsException()
+    return random.choice(list_of_words)
 
 
 def _mask_word(word):
-    pass
+    if not word:
+        raise InvalidWordException()
+    return '*' * len(word)
 
 
 def _uncover_word(answer_word, masked_word, character):
-    pass
+    if not answer_word or not masked_word:
+        raise InvalidWordException()
+
+    if len(character) > 1:
+        raise InvalidGuessedLetterException()
+
+    if len(answer_word) != len(masked_word):
+        raise InvalidWordException()
+
+    answer = answer_word.lower()
+    if character.lower() not in answer:
+        return masked_word
+
+    new_word = ''
+
+    for answer_char, masked_char in zip(answer, masked_word):
+        if character.lower() == answer_char:
+            new_word += answer_char
+        else:
+            new_word += masked_char
+
+    return new_word
+
+
+def _is_game_won(game):
+    return game['answer_word'].lower() == game['masked_word'].lower()
+
+
+def _is_game_lost(game):
+    return game['remaining_misses'] <= 0
+
+
+def _is_game_finished(game):
+    return _is_game_lost(game) or _is_game_won(game)
 
 
 def guess_letter(game, letter):
